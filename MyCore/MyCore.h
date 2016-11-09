952dd76e97cc3f3cd0090bc3b5adf976616e0aeb
ZezhongLi <neil.zezhong.li@gmail.com>
2016-11-09 00:31:17 -0500

    initial commit

---
+++ MyCore/MyCore.h
@@ -0,0 +1,19 @@
+//
+//  MyCore.h
+//  MyCore
+//
+//  Created by lee Neil on 11/9/16.
+//  Copyright © 2016 Neil's Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyCore.
+FOUNDATION_EXPORT double MyCoreVersionNumber;
+
+//! Project version string for MyCore.
+FOUNDATION_EXPORT const unsigned char MyCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCore/PublicHeader.h>
+
+
---
+++ MyCore/SomeObject.swift
@@ -0,0 +1,14 @@
+//
+//  SomeObject.swift
+//  MyMainApp
+//
+//  Created by lee Neil on 11/9/16.
+//  Copyright © 2016 Neil's Studio. All rights reserved.
+//
+
+import UIKit
+
+public class SomeObject: NSObject {
+    public var sharable = false
+
+}
