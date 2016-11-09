417dc0a12a931c583bbb0e3a0c9c22875d332df6
ZezhongLi <neil.zezhong.li@gmail.com>
2016-11-09 00:24:13 -0500

    Initial Commit

---
+++ MyMainApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMainApp
+//
+//  Created by lee Neil on 11/9/16.
+//  Copyright © 2016 Neil's Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
